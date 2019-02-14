9e6bc504d4c5b8a2be78bf86de4366177836c81f
Tiwari,Sagar <s527999@nw43143.nwmissouri.edu>
2019-02-14 16:09:28 -0600

    Initial Commit

---
+++ ArithMEtic App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArithMEtic App
+//
+//  Created by Tiwari,Sagar on 2/14/19.
+//  Copyright © 2019 Tiwari,Sagar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
